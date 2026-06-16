9547281db9417df7506dcabb1a3eb9d45f22f3d3
Atharv Yadv <ravi.burman@xperate.com>
2026-06-16 12:41:48 +0530

    Refactor SettingsPage component and clean up package.json by removing unused dependencies and scripts

--- app/src/app/settings/SettingsPage.tsx
+++ app/src/app/settings/SettingsPage.tsx
@@ -1,39 +1,15 @@
-import { EmptyState, Link, Text } from '@hubspot/ui-extensions';
-import {
-  hubspot,
-  ExtensionPointApiActions,
-  SettingsContext,
-} from '@hubspot/ui-extensions';
+import React from "react";
+import { EmptyState, Text } from "@hubspot/ui-extensions";
+import { hubspot } from "@hubspot/ui-extensions";
 
-interface SettingsExtensionProps {
-  context: SettingsContext;
-  actions: ExtensionPointApiActions<'settings'>;
-}
+hubspot.extend(({ context }) => {
+  return <NewSettingsPage context={context} />;
+});
 
-hubspot.extend<'settings'>(({ context, actions }: SettingsExtensionProps) => (
-  <SettingsPage context={context} actions={actions} />
-));
-
-const SettingsPage = ({ context, actions }: SettingsExtensionProps) => {
-  console.log({ context });
-
-  const appSettingsDocsLink =
-    'https://developers.hubspot.com/docs/apps/developer-platform/add-features/ui-extensibility/create-a-settings-component';
-
+const NewSettingsPage = ({ context }) => {
   return (
-    <>
-      <EmptyState
-        title="Build your Settings page here!"
-        layout="horizontal"
-        imageName="building"
-      >
-        <Text>
-          Add configuration and customization settings for your app, linked to
-          directly from your app cards. Check out the{' '}
-          <Link href={appSettingsDocsLink}>app settings documentation</Link> for
-          more info.
-        </Text>
-      </EmptyState>
-    </>
+    <EmptyState title="Nothing here yet!" layout="vertical">
+      <Text>Build your application settings page here!</Text>
+    </EmptyState>
   );
 };
